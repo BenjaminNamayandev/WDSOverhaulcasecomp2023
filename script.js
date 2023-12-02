78d085d644c51cd014b90923798acdc1f974e563
BenjaminNamayandeh <113051650+BenjaminNamayandeh@users.noreply.github.com>
2023-12-02 14:06:53 -0500

    Login Page restored

--- script.js
+++ script.js
@@ -48,3 +48,45 @@ function logout(event) {
     localStorage.removeItem('password');
     window.location.href = 'login.html';
 }
+
+function translateText() {
+    const inputTextarea = document.getElementById('writing');
+    const selectLanguage = document.getElementById('selectLanguage');
+    const outputText = document.getElementById('outputText');
+
+    const apiKey = 'AIzaSyDJ5ou-M_JhYJi0HMkLOzq4v9Kfje-Frio'; 
+    const targetLanguage = selectLanguage.value;
+    const textToTranslate = inputTextarea.value;
+
+    fetch(`https://translation.googleapis.com/language/translate/v2?key=${apiKey}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        q: textToTranslate,
+        target: targetLanguage,
+      }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      const translatedText = data.data.translations[0].translatedText;
+      inputTextarea.value = translatedText;
+    })
+    .catch(error => {
+      console.error('Error translating text:', error);
+    });
+  }
+
+function convertToSpeech() {
+    const writing = document.getElementById('writing').value;
+
+    if ('speechSynthesis' in window) {
+        const synthesis = window.speechSynthesis;
+        const utterance = new SpeechSynthesisUtterance(writing);
+        synthesis.speak(utterance);
+    
+    } else {
+        alert('Text-to-speech is not supported in your browser. Please use a modern browser.');
+    }
+  }

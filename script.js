1c6bfbd93dce39cfdd290c29117d685fa3f34d45
BenjaminNamayandeh <113051650+BenjaminNamayandeh@users.noreply.github.com>
2023-12-02 21:54:44 -0500

    Finally...

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", attachListeners);
 document.addEventListener("DOMContentLoaded", checkPage);
 
+document.getElementById("writing").disabled = true;
+
 // Initialize current file name
 let currentFileName = '';
 
@@ -24,7 +26,7 @@ function addFile() {
     let filebar = document.querySelector('.files');
     let newFileButton = document.createElement('button');
     let fileName = window.prompt("Name the file:", "Enter name here");
-    
+
     // Check if the file name is not empty
     if (fileName) {
         newFileButton.textContent = fileName;
@@ -32,6 +34,7 @@ function addFile() {
             // Set current file name when switching files
             currentFileName = fileName;
             loadNotes();
+            document.getElementById("writing").disabled = false;
         });
         filebar.appendChild(newFileButton);
 
@@ -47,6 +50,31 @@ function loadFiles() {
     // Clear existing file buttons
     filebar.innerHTML = '';
 
+    if (files.length === 0) {
+        let newFileButton = document.createElement('button');
+        let fileName = 'My First File';
+
+        newFileButton.textContent = fileName;
+        newFileButton.addEventListener('click', () => {
+            // Set current file name when switching files
+            currentFileName = fileName;
+            loadNotes();
+            document.getElementById("writing").disabled = false;
+
+        });
+        filebar.appendChild(newFileButton);
+
+        // Save files to localStorage
+        saveFiles();
+
+        // Set the notes variable to 'My First File'
+        let notes = JSON.parse(localStorage.getItem(currentFileName)) || '';
+
+        // Load notes after setting the value
+        loadNotes();
+        document.getElementById("writing").disabled = false;
+    }
+
     // Load files from localStorage
     files.forEach(fileName => {
         let newFileButton = document.createElement('button');
@@ -55,10 +83,12 @@ function loadFiles() {
             // Set current file name when switching files
             currentFileName = fileName;
             loadNotes();
+            document.getElementById("writing").disabled = false;
         });
         filebar.appendChild(newFileButton);
     });
-}
+
+  }
 
 function saveFiles() {
     let files = Array.from(document.querySelectorAll('.files button')).map(button => button.textContent);
@@ -76,11 +106,17 @@ function saveNotes() {
 }
 
 function checkPage() {
-    let username = localStorage.getItem('username');
-    if (window.location.href.includes('login.html') && username)
-        window.location = 'notes.html';
-    if (window.location.href.includes('notes.html') && !username)
-        window.location = 'login.html';
+  let username = localStorage.getItem('username');
+  if (window.location.href.includes('login.html') && username)
+      window.location = 'notes.html';
+  if (window.location.href.includes('notes.html') && !username) {
+      // Load into 'My First File' if no existing files
+      if (JSON.parse(localStorage.getItem('files')).length === 0) {
+          currentFileName = 'My First File';
+          loadNotes();
+      }
+      window.location = 'login.html';
+  }
 }
 
 function login(event) {
@@ -100,36 +136,35 @@ function logout(event) {
     localStorage.removeItem('username');
     localStorage.removeItem('password');
     window.location.href = 'login.html';
-} 
+}
 
 function translateText() {
     const inputTextarea = document.getElementById('writing');
     const selectLanguage = document.getElementById('selectLanguage');
-    const outputText = document.getElementById('outputText');
 
-    const apiKey = 'AIzaSyDJ5ou-M_JhYJi0HMkLOzq4v9Kfje-Frio'; 
+    const apiKey = 'AIzaSyDJ5ou-M_JhYJi0HMkLOzq4v9Kfje-Frio';
     const targetLanguage = selectLanguage.value;
     const textToTranslate = inputTextarea.value;
 
     fetch(`https://translation.googleapis.com/language/translate/v2?key=${apiKey}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        q: textToTranslate,
-        target: targetLanguage,
-      }),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            q: textToTranslate,
+            target: targetLanguage,
+        }),
     })
-    .then(response => response.json())
-    .then(data => {
-      const translatedText = data.data.translations[0].translatedText;
-      inputTextarea.value = translatedText;
-    })
-    .catch(error => {
-      console.error('Error translating text:', error);
-    });
-  }
+        .then(response => response.json())
+        .then(data => {
+            const translatedText = data.data.translations[0].translatedText;
+            inputTextarea.value = translatedText;
+        })
+        .catch(error => {
+            console.error('Error translating text:', error);
+        });
+}
 
 function convertToSpeech() {
     const writing = document.getElementById('writing').value;
@@ -138,11 +173,11 @@ function convertToSpeech() {
         const synthesis = window.speechSynthesis;
         const utterance = new SpeechSynthesisUtterance(writing);
         synthesis.speak(utterance);
-    
+
     } else {
         alert('Text-to-speech is not supported in your browser. Please use a modern browser.');
     }
-  }
+}
 
 // Event listener for writing text area changes
 document.getElementById('writing').addEventListener('input', saveNotes);
